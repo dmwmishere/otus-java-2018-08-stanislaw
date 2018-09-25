2278bf6e1fe9413c2000865da45ff8ae408a80b9
Stanislaw <dmwm88@gmail.com>
2018-09-25 22:44:56 +0300

    HW4 fixes

--- shw4/src/main/java/ru/otus/TestFrameworkProcessor.java
+++ shw4/src/main/java/ru/otus/TestFrameworkProcessor.java
@@ -8,26 +8,18 @@ package ru.otus;
 2. именем package в котором надо найти и запустить тесты
  */
 
-import ru.otus.annotations.After;
-import ru.otus.annotations.Before;
-import ru.otus.annotations.Test;
 import ru.otus.annotations.TestCase;
 
 import java.io.File;
-import java.lang.annotation.Annotation;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class TestFrameworkProcessor {
 
     public static void runTests(String packageName) throws ClassNotFoundException {
         List<String> classes = getClassesFromPackage(packageName);
-        if(classes == null){
+        if (classes == null) {
             System.err.println("NO CLASSES FOUND IN " + packageName);
             return;
         }
@@ -42,87 +34,34 @@ public class TestFrameworkProcessor {
         }
     }
 
-    public static void runTests(Class<?> ... testCaseClasses) {
-
-        int failed = 0, passed = 0, skipped = 0;
-        for(Class<?> testCaseClass : testCaseClasses) {
-            if (testCaseClass.isAnnotationPresent(TestCase.class)) {
-
-                List<Method> before = methodsByAnnotation(testCaseClass, Before.class);
-
-                List<Method> testStep = methodsByAnnotation(testCaseClass, Test.class);
-                testStep.sort((m1, m2) -> { // execute test steps in defined order
-                    int order1 = m1.getAnnotation(Test.class).order(),
-                            order2 = m2.getAnnotation(Test.class).order();
-
-                    if (order1 == order2) return 0;
-                    return order1 < order2 ? -1 : 1;
-                });
-
-                List<Method> after = methodsByAnnotation(testCaseClass, After.class);
+    public static void runTests(Class<?> testCaseClass) {
 
 
-                try {
-                    Object obj = testCaseClass.newInstance();
+        if (testCaseClass.isAnnotationPresent(TestCase.class)) {
 
-                    for (Method method : before) {
-                        // skip test case execution if there are any errors before test steps
-                        if (!executeTest(method, obj)) return;
-                    }
+            TestCaseRunner tcr = new TestCaseRunner(testCaseClass);
 
-                    for (Method method : testStep) {
-                        if (failed > 0) { // skip all steps after any failures occurred
-                            skipped++;
-                        } else {
-                            if (executeTest(method, obj)) passed++;
-                            else failed++;
-                        }
-                    }
+            tcr.runTest();
+            int failed = tcr.getFailed(), passed = tcr.getPassed(), skipped = tcr.getSkipped();
+            System.out.printf("==============================================================\n" +
+                    "Total tests = %d, \u001B[31mFailed = %d, \u001B[32mPassed = %d, \u001B[33mskipped = %d\n\u001B[0m" +
+                    "==============================================================\n", failed + passed + skipped, failed, passed, skipped);
 
-                    for (Method method : after) {
-                        executeTest(method, obj);
-                    }
-
-                } catch (IllegalAccessException | InstantiationException e) {
-                    e.printStackTrace();
-                    return;
-                }
-
-                System.out.printf("==============================================================\n" +
-                        "Total tests = %d, \u001B[31mFailed = %d, \u001B[32mPassed = %d, \u001B[33mskipped = %d\n\u001B[0m" +
-                        "==============================================================\n", failed + passed + skipped, failed, passed, skipped);
-
-            } else {
-                System.out.println(testCaseClass.getName() + " is not a test case. skip.");
-            }
+        } else {
+            System.out.println(testCaseClass.getName() + " is not a test case. skip.");
         }
-    }
-
-    private static boolean executeTest(Method method, Object instance) throws IllegalAccessException {
-        try {
-            method.invoke(instance);
-            return true;
-        } catch (InvocationTargetException e) {
-            e.printStackTrace();
-            System.err.printf("test step %s failed with error: %s\n", method.getName(), e.getCause());
-            return false;
-        }
-    }
-
-    private static List<Method> methodsByAnnotation(Class<?> cls, Class<? extends Annotation> annotation) {
-        return Stream.of(cls.getDeclaredMethods()).filter(m -> m.isAnnotationPresent(annotation)).collect(Collectors.toList());
     }
 
     private static ArrayList<String> getClassesFromPackage(final String packageName) {
         ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
         ArrayList<String> names = new ArrayList<>();
         URL url = classLoader.getResource(packageName.replace(".", "/"));
-        if(url == null){
+        if (url == null) {
             System.err.println("No such package " + packageName);
             return null;
         }
         File[] classFiles = new File(url.toString().replace("file:", "")).listFiles();
-        if(classFiles != null) {
+        if (classFiles != null) {
             for (File actual : classFiles) {
                 String clazz = actual.getName();
                 clazz = packageName + "." + clazz.substring(0, clazz.lastIndexOf('.'));
