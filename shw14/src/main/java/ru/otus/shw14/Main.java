569b9c5ef6e43ff0aeead83ba1aaf42af7f3d2ae
Stanislaw <dmwm88@gmail.com>
2019-01-14 21:45:13 +0300

    HomeWork 14 added

---
+++ shw14/src/main/java/ru/otus/shw14/Main.java
@@ -0,0 +1,17 @@
+package ru.otus.shw14;
+
+/**
+ * ДЗ-14: WAR
+ * Собрать war для приложения из ДЗ-12.
+ * Создавать кэш и DBService как Spring beans, передавать (inject) их в сервлеты.
+ * Запустить веб приложение во внешнем веб сервере.
+ */
+
+public class Main {
+
+    public static void main(String [] argz) throws Exception{
+
+    }
+
+
+}
---
+++ shw14/src/main/java/ru/otus/shw14/springWrappedServlets/BrowseUserServlet.java
@@ -0,0 +1,62 @@
+package ru.otus.shw14.springWrappedServlets;
+
+import ru.otus.shw10.base.DBService;
+import ru.otus.shw10.data.PhoneDataSet;
+import ru.otus.shw10.data.UserDataSet;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class BrowseUserServlet extends HttpServlet {
+
+    private DBService dbSrv;
+
+    private TemplateProcessor templateProcessor;
+
+    @Override
+    public void init(){
+        dbSrv = SpringContextListener.getSpringBean("dbSrv", DBService.class);
+        templateProcessor = SpringContextListener.getSpringBean("tmpPrc", TemplateProcessor.class);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws IOException {
+        List<UserDataSet> users = dbSrv.readAll();
+        System.out.println("User count = " + users.size());
+        System.out.println("User = " + users.get(0));
+
+        List<Map<String, Object> > usersData = new ArrayList<>();
+
+        users.forEach(user -> usersData.add(mapUserToMap(user)));
+
+        Map<String, Object> pageVariables = new HashMap<>();
+        pageVariables.put("userCount", users.size());
+        pageVariables.put("users", usersData);
+        String page = templateProcessor.getPage("browseuser2.html", pageVariables);
+        resp.setCharacterEncoding("UTF-8");
+        resp.getWriter().println(page);
+        resp.setStatus(HttpServletResponse.SC_OK);
+
+    }
+
+    private Map<String, Object> mapUserToMap(UserDataSet user){
+        Map<String, Object> userData = new HashMap<>();
+        userData.put("id", user.getId());
+        userData.put("name", user.getName());
+        userData.put("age", user.getAge());
+        userData.put("address", user.getAdress().getStreet());
+        userData.put("phones", String.join(", ", user.getPhones().stream()
+                .map(PhoneDataSet::getPhone).collect(Collectors.toList())));
+        return userData;
+
+    }
+
+}
